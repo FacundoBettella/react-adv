911c211f6955dc7b027462a08395bc95a06b85a2
facundobettella <facundobettella@outlook.com>
2023-02-02 18:09:19 -0300

    new branch

--- src/02-component-patterns/pages/Shopping.tsx
+++ src/02-component-patterns/pages/Shopping.tsx
@@ -1,5 +1,3 @@
-import "../styles/custom-styles.css";
-import styles from "../styles/styles.module.css";
 import { ProductCard } from '../components';
 import { products } from "../data/products";
 
@@ -10,49 +8,23 @@ const Shopping = () => {
       <h1>Shopping Store</h1>
       <hr style={{ width: "100%" }} />
 
-      <div className="shopping-div">
-        <ProductCard
-          product={products[2]}
-          className='bg-dark'
-          style={{ backgroundColor: "#6bb6d3" }}
-
-          initialValues={{
-            count: 5,
-            maxCount: 10,
-          }}
-        >
-          {
-            ({ isMaxCountReached, count, reset, increaseBy }) => (
-              <>
-                <ProductCard.Image className="custom-image" />
-                <ProductCard.Title className='text-white text-bold' />
-                <ProductCard.Buttons className="custom-button" style={{ display: "flex", justifyContent: "flex-end" }} />
-                {/* { JSON.stringify(args, null, 3) } */}
-
-                <div
-                  style={{
-                    color: "white",
-                    display: "flex",
-                    justifyContent: "flex-end",
-                    gap: 6,
-                    padding: 8,
-                  }}>
-
-                  <button onClick={() => increaseBy(-2)}> -2 </button>
-                  {
-                    !isMaxCountReached &&
-                    <button onClick={() => increaseBy(+2)}> +2 </button>
-                  }
-                  <span className={styles.countLabel}>{count}</span>
-
-                  <button onClick={reset}> Reset </button>
-                </div>
-              </>
-
-            )
-          }
-        </ProductCard>
-      </div>
+      <ProductCard
+        product={products[2]}
+        initialValues={{
+          count: 5,
+          maxCount: 10,
+        }}
+      >
+        {
+          ({ isMaxCountReached, count, reset, increaseBy }) => (
+            <>
+              <ProductCard.Image />
+              <ProductCard.Title />
+              <ProductCard.Buttons />
+            </>
+          )
+        }
+      </ProductCard>
     </>
   )
 }
