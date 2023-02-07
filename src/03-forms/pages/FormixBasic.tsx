40936558cb41e910b0d923ac3f95f3802be2c735
facundobettella <facundobettella@outlook.com>
2023-02-06 21:09:17 -0300

    Added: Formik basic form

--- src/03-forms/pages/FormixBasic.tsx
+++ src/03-forms/pages/FormixBasic.tsx
@@ -1,17 +1,50 @@
 import "../styles/styles.css";
 import { FC } from "react";
-import { useFormik } from "formik";
+import { useFormik, FormikErrors } from "formik";
+
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
 export const FormikBasic: FC = () => {
-  const { values, handleChange, handleSubmit } = useFormik({
+  const validate = ({ firstName, lastName, email }: FormValues) => {
+    const errors: FormikErrors<FormValues> = {};
+
+    if (!firstName) {
+      errors.firstName = "Required";
+    } else if (firstName.length >= 15) {
+      errors.firstName = "Must be 15 characters or less";
+    }
+
+    if (!lastName) {
+      errors.lastName = "Required";
+    } else if (lastName.length >= 10) {
+      errors.lastName = "Must be 10 characters or less";
+    }
+
+    if (!email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      errors.email = "Invalid email address";
+    }
+
+    return errors;
+  };
+
+  const { values, handleChange, handleSubmit, errors, touched, handleBlur } = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
       email: "",
     },
+
     onSubmit: (values) => {
       console.log(values);
     },
+
+    validate: validate,
   });
 
   return (
@@ -25,7 +58,11 @@ export const FormikBasic: FC = () => {
           name='firstName'
           value={values.firstName}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {touched.firstName && errors.firstName && (
+          <span>{errors.firstName}</span>
+        )}
 
         <label htmlFor='lastName'>Last name</label>
         <input
@@ -33,7 +70,9 @@ export const FormikBasic: FC = () => {
           name='lastName'
           value={values.lastName}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {touched.lastName && errors.lastName && <span>{errors.lastName}</span>}
 
         <label htmlFor='email'>Email Address</label>
         <input
@@ -41,7 +80,9 @@ export const FormikBasic: FC = () => {
           name='email'
           value={values.email}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {touched.email && errors.email && <span>{errors.email}</span>}
 
         <button type='submit'>Submit</button>
       </form>
